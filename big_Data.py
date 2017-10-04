9c45f3cebbabbbfded305eec2213c7f27704ca64
Joostluijben <joost.luijben@student.hu.nl>
2017-10-04 13:51:35 +0200

    Some changes

--- big_Data.py
+++ big_Data.py
@@ -9,14 +9,14 @@ naam = ('Bas', 'Joost', 'Piet', 'Klaas', 'Bart', 'Henk', 'Maartje', 'Anne', 'Mee
 merk = ('Hyundai', 'Volvo', 'Ford', 'Volkswagen', 'Peugot', 'Citroen', 'Honda', 'Skoda', 'Ferrari', 'Audi')
 deur = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 leeftijd = (20, 30, 40, 50, 60, 70, 80, 90, 110, 120)
-vloeistof = ['koffie', 'chocomel', 'water', 'limonade', 'thee', 'whiskey', 'wodka', 'cola', 'ice_tea', 'fanta']
-snelheid = [40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
-tankstation = ['Shell', 'BP', 'Esso', 'Total', 'Texaco', 'Q8', 'Caltex', 'Haan', 'ARCO', 'American Gas']
-raam = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+vloeistof = ('koffie', 'chocomel', 'water', 'limonade', 'thee', 'whiskey', 'wodka', 'cola', 'ice_tea', 'fanta')
+snelheid = (40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140)
+tankstation = ('Shell', 'BP', 'Esso', 'Total', 'Texaco', 'Q8', 'Caltex', 'Haan', 'ARCO', 'American Gas')
+raam = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
 
 
 def writeCSV():
-    with open('file.csv', 'w', newline='\n') as csvFile:
+    with open('file1.csv', 'w', newline='\n') as csvFile:
         fieldnames = (
             'Woonplaats', 'Kleur', 'Naam', 'Merk', 'Deuraantal', 'Leeftijd', 'Vloeistof', 'Snelheid', 'Tankstation',
             'Ramen')
@@ -42,20 +42,49 @@ def writeCSV():
                                  'Vloeistof': vloeistof[number], 'Snelheid': snelheid[number],
                                  'Tankstation': tankstation[number], 'Ramen': raam[number]})
                 line += 1
-            elif number not in range(len(naam)):
-                # continue
-                print('error')
-                break
+            elif number not in range(len(woonplaats)):
+                continue
+                # print('error')
+                # break
 
 
-writeCSV()
+# writeCSV()
+
 
 def calculate():
     with open('file.csv', 'r', newline='\n') as csvread:
         reader = csv.reader(csvread)
         next(reader)
+        dic = {'Woonplaats': {}, 'Kleur': {}}
         for x in reader:
-            int(x[1].count('rood'))
+            if x[0] in dic['Woonplaats']:
+                dic['Woonplaats'][x[0]] += 1
+            elif x[0] not in dic['Woonplaats']:
+                dic['Woonplaats'][x[0]] = 1
+
+            if x[1] in dic['Kleur']:
+                dic['Kleur'][x[1]] += 1
+            elif x[1] not in dic['Kleur']:
+                dic['Kleur'][x[1]] = 1
+
+        print(dic)
 
 
 # calculate()
+dic = {}
+
+with open('test.csv', 'r', newline='\n') as testCSV:
+    testReader = csv.reader(testCSV)
+    for item in next(testReader):
+        dic[item] = {}
+    print(dic)
+    for row in testReader:
+        for indexNumber in range(len(row)):
+            if row[indexNumber] in dic:
+                for key in dic.keys():
+                    dic[key][row[indexNumber]] += 1
+            elif row[indexNumber] not in dic:
+                for key in dic.keys():
+                    dic[key][row[indexNumber]] = 0
+    for x in dic.keys():
+        print(x)
