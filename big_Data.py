7da0e881f0c1a3018754a8ba3213283bfd85d57c
Joostluijben <joost.luijben@student.hu.nl>
2017-10-04 23:02:32 +0200

    Some changes to the writer, making it more random and compact code

--- big_Data.py
+++ big_Data.py
@@ -1,22 +1,22 @@
 import csv
 import random
 
-woonplaats = (
-    'Bunnik', 'Amsterdam', 'Utrecht', 'Eindhoven', 'Rotterdam', 'Nijmegen', 'Groningen', 'Leeuwarden', 'Woerden',
-    'Delft')
-kleur = ('rood', 'blauw', 'geel', 'groen', 'zwart', 'wit', 'paars', 'oranje', 'grijs', 'limoen')
-naam = ('Bas', 'Joost', 'Piet', 'Klaas', 'Bart', 'Henk', 'Maartje', 'Anne', 'Mees', 'Pom')
-merk = ('Hyundai', 'Volvo', 'Ford', 'Volkswagen', 'Peugot', 'Citroen', 'Honda', 'Skoda', 'Ferrari', 'Audi')
-deur = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-leeftijd = (20, 30, 40, 50, 60, 70, 80, 90, 110, 120)
-vloeistof = ('koffie', 'chocomel', 'water', 'limonade', 'thee', 'whiskey', 'wodka', 'cola', 'ice_tea', 'fanta')
-snelheid = (40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140)
-tankstation = ('Shell', 'BP', 'Esso', 'Total', 'Texaco', 'Q8', 'Caltex', 'Haan', 'ARCO', 'American Gas')
-raam = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+tup = ((
+           'Bunnik', 'Amsterdam', 'Utrecht', 'Eindhoven', 'Rotterdam', 'Nijmegen', 'Groningen', 'Leeuwarden', 'Woerden',
+           'Delft'),
+       ('rood', 'blauw', 'geel', 'groen', 'zwart', 'wit', 'paars', 'oranje', 'grijs', 'limoen'),
+       ('Bas', 'Joost', 'Piet', 'Klaas', 'Bart', 'Henk', 'Maartje', 'Anne', 'Mees', 'Pom'),
+       ('Hyundai', 'Volvo', 'Ford', 'Volkswagen', 'Peugot', 'Citroen', 'Honda', 'Skoda', 'Ferrari', 'Audi'),
+       (0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+       (20, 30, 40, 50, 60, 70, 80, 90, 110, 120),
+       ('koffie', 'chocomel', 'water', 'limonade', 'thee', 'whiskey', 'wodka', 'cola', 'ice_tea', 'fanta'),
+       (40, 50, 60, 70, 80, 90, 100, 110, 120, 130),
+       ('Shell', 'BP', 'Esso', 'Total', 'Texaco', 'Q8', 'Caltex', 'Haan', 'ARCO', 'American Gas'),
+       (2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
 
 
 def writeCSV():
-    with open('file1.csv', 'w', newline='\n') as csvFile:
+    with open('file.csv', 'w', newline='\n') as csvFile:
         fieldnames = (
             'Woonplaats', 'Kleur', 'Naam', 'Merk', 'Deuraantal', 'Leeftijd', 'Vloeistof', 'Snelheid', 'Tankstation',
             'Ramen')
@@ -32,23 +32,26 @@ def writeCSV():
         #      break
 
         writer.writeheader()
-        line = 0
-        while line < 100000:  # while line number is not reached, keep writing rows
-            number = random.randrange(10)
-            if number in range(len(woonplaats)):  # if the random generated indexnumber
-                # does not appear in the indexrange of the tuples, try again
-                writer.writerow({'Woonplaats': woonplaats[number], 'Kleur': kleur[number], 'Naam': naam[number],
-                                 'Merk': merk[number], 'Deuraantal': deur[number], 'Leeftijd': leeftijd[number],
-                                 'Vloeistof': vloeistof[number], 'Snelheid': snelheid[number],
-                                 'Tankstation': tankstation[number], 'Ramen': raam[number]})
-                line += 1
-            elif number not in range(len(woonplaats)):
-                continue
-                # print('error')
-                # break
+        # old code, please ignore
+        # line = 0
+        # while line < 100000:  # while line number is not reached, keep writing rows
+        #    number = random.randrange(10)
+        #    if number in range(len(kleur)):  # if the random generated indexnumber
+        #        # does not appear in the indexrange of the tuples, try again
+        #        writer.writerow({'Woonplaats': woonplaats[number], 'Kleur': kleur[number], 'Naam': naam[number],
+        #                         'Merk': merk[number], 'Deuraantal': deur[number], 'Leeftijd': leeftijd[number],
+        #                         'Vloeistof': vloeistof[number], 'Snelheid': snelheid[number],
+        #                         'Tankstation': tankstation[number], 'Ramen': raam[number]})
+        #        line += 1
+        #    elif number not in range(len(kleur)):
+        #        continue
+
+        for line in range(100000):
+            writer.writerow({fieldnames[i]: tup[i][random.randrange(10)] for i in range(10)})
 
 
-# writeCSV()
+
+writeCSV()
 
 
 def calculate():
@@ -70,21 +73,17 @@ def calculate():
         print(dic)
 
 
-# calculate()
+'''# calculate()
 dic = {}
-
+count = 0
 with open('test.csv', 'r', newline='\n') as testCSV:
     testReader = csv.reader(testCSV)
     for item in next(testReader):
         dic[item] = {}
     print(dic)
     for row in testReader:
+        'print(row)'
         for indexNumber in range(len(row)):
-            if row[indexNumber] in dic:
-                for key in dic.keys():
-                    dic[key][row[indexNumber]] += 1
-            elif row[indexNumber] not in dic:
-                for key in dic.keys():
-                    dic[key][row[indexNumber]] = 0
-    for x in dic.keys():
-        print(x)
+            print(row[indexNumber])
+
+    print(dic)'''
